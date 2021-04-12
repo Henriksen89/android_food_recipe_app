438136557cb9dd5792225c5826b4adbfc1125cc2
Victor <vicle18@student.sdu.dk>
2021-04-12 20:53:06 +0200

    setting up ingredients- and instructionsfragments

--- app/src/main/java/com/example/models/Recipe.kt
+++ app/src/main/java/com/example/models/Recipe.kt
@@ -1,7 +1,11 @@
 package com.example.models
 
-import android.widget.ImageView
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Recipe(val image: Int, val recipeTitle: String, val recipeDescription: String)
+data class Recipe(val image: Int,
+                  val recipeTitle: String,
+                  val recipeDescription: String,
+                  )
+//val ingredients : ArrayList<String>,
+//val instructions : ArrayList<String>
--- app/src/main/java/com/example/views/showRecipe/RecipeInstructionsFragment.kt
+++ app/src/main/java/com/example/views/showRecipe/RecipeInstructionsFragment.kt
@@ -4,16 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.example.food_recipe_app.R
 import com.example.models.Recipe
 
 class RecipeInstructionsFragment(var recipe: Recipe) : Fragment(){
+    private lateinit var imageView: ImageView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.tab_instructions_fragment, container, false)
+        val view : View = inflater.inflate(R.layout.tab_instructions_fragment, container, false)
+        setUpRecipe(view)
+        return view
     }
+
+    fun setUpRecipe(view: View){
+        imageView = view.findViewById(R.id.imageView_instructions)
+        imageView.setImageResource(recipe.image)
+    }
+
 }
