e0f1e748c93620e45e3e598bbc99c80d9e1fc610
Henriksen89 <rehen17@student.sdu.dk>
2021-04-13 15:06:22 +0200

    added bottom_nav_menu

--- app/src/main/java/com/example/views/MainActivity.kt
+++ app/src/main/java/com/example/views/MainActivity.kt
@@ -2,15 +2,35 @@ package com.example.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.food_recipe_app.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity()  {
+    private lateinit var navController: NavController
    // private val recipeViewModel: RViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
        // recipeViewModel.repository = (application as RecipeApplication).repository
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.navHostFragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.recipeFragment,
+                R.id.fragmentFavorites))
+
+        bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
